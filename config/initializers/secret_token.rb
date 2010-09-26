4557b53d14e8415fe90d684a7a5a24982ec490a1
yalab <rudeboyjet@gmail.com>
2010-09-26 12:01:12 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anmitsu::Application.config.secret_token = '565fc67df45f662588290e604d7b0ff93098da40f76c751675d5a32fffc6c4e6c77f69310cd71604a399d972def6f5ee53e793ff9cbf83d326349c79e6c6e971'
