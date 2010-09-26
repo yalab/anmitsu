4bc0b932b4e6f43df4bebabce5c8b4783877b74f
yalab <rudeboyjet@gmail.com>
2010-09-26 14:06:38 +0900

    Create login function with devise.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(:email => 'admin@example.com', :password => (pass = 'password'), :password_confirmation => pass) if User.count < 0
+User.create(:email => 'admin@example.com', :password => (pass = 'password'), :password_confirmation => pass).confirm!
