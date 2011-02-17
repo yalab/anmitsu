1887c21dfba047d77c2ad5d03a54eb9bff64993e
yalab <rudeboyjet@gmail.com>
2011-02-17 10:48:45 +0900

    Remove Item#estimation.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item
   include Mongoid::Document
   include Mongoid::Timestamps
   include Stateflow
-  [:title, :description, :note, :client_name, :state, :expire].each{|name| field name, :type => String }
+  [:title, :description, :note, :client_name, :state].each{|name| field name, :type => String }
   [:created_at].each{|name| field name, :type => Time }
   referenced_in :user
   embeds_many :accounts
