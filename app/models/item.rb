3f7e01a30a0cac9925b576091e709f15e9fddd8b
yalab <rudeboyjet@gmail.com>
2011-02-17 13:28:41 +0900

    Add sub total on items/show.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item
   include Mongoid::Document
   include Mongoid::Timestamps
   include Stateflow
+  TAX = 0.5
   [:title, :description, :note, :client_name, :state].each{|name| field name, :type => String }
   [:created_at].each{|name| field name, :type => Time }
   referenced_in :user
@@ -15,5 +16,8 @@ class Item
 
   def total
     accounts.map{|account| account.price }.inject(:+)
+  end
+  def total_with_tax
+    (total * TAX).truncate
   end
 end
--- public/javascripts/items/show.js
+++ public/javascripts/items/show.js
@@ -1,62 +1,62 @@
 $(function(){
     $("#account_submit").click(function(){
-                                 $(this).hide();
-                                 $("#ajax_loader").show();
-                                 var form = $("#new_account");
-                                 $.ajax({url: form.attr('action'),
-                                         type: 'POST',
-                                         data: form.serialize(),
-                                         success: function(account){
-                                           $(":text").val('');
-                                           $(".alert").val('');
-                                           $("#ajax_loader").hide();
-                                           $("#account_submit").show();
-                                           var tr = $(document.createElement('tr'));
-                                           tr.attr('id', 'account_' + account._id);
-                                           var fields = ['content', 'price'];
-                                           for(var n = 0; n < fields.length; n++){
-                                             var td = $(document.createElement('td'));
-                                             var k = fields[n];
-                                             if(k == 'price'){
-                                               td.css('text-align', 'right');
-                                             }
-                                             td.html(account[k]);
-                                             tr.append(td);
-                                           }
-                                           td = $(document.createElement('td'));
-                                           var link = $(document.createElement('a'));
-                                           var href = [location.pathname, "accounts", account._id].join('/');
-                                           link.attr({'href'       : href,
-                                                      'data-method': "delete",
-                                                      'data-remote': "true",
-                                                      'rel'        : "nofollow"});
-                                           link.html("[削除]");
-                                           link.bind('ajax:success', delete_success);
-                                           link.click(function(e){
-                                                        $(this).callRemote();
-                                                        e.preventDefault();
-                                                      });
-                                           td.append(link);
-                                           tr.append(td);
-                                           $("#estimates_table").append(tr);
-                                           tr.hide();
-                                           tr.fadeIn('slow');
-                                         },
-                                         error: function(res){
-                                           var errors = $.parseJSON(res.responseText);
-                                           for(k in errors){
-                                             $("#"+k+'_error').html(errors[k]);
-                                           }
-                                           $("#ajax_loader").hide();
-                                           $("#account_submit").show();
-                                         }
-                                        });
-                                 return false;
-                               });
-    var delete_success = function(){
-      var id = this.href.split('/').pop();
-      var node = $("#account_"+ id);
-      node.fadeOut('slow', function(){ node.remove(); });
-    };
-    $("a[data-method=delete]").bind('ajax:success', delete_success);
-  });
+      $(this).hide();
+      $("#ajax_loader").show();
+      var form = $("#new_account");
+      $.ajax({url: form.attr('action'),
+              type: 'POST',
+              data: form.serialize(),
+              success: function(account){
+                $(":text").val('');
+                $(".alert").val('');
+                $("#ajax_loader").hide();
+                $("#account_submit").show();
+                var tr = $(document.createElement('tr'));
+                tr.attr('id', 'account_' + account._id);
+                var fields = ['content', 'price'];
+                for(var n = 0; n < fields.length; n++){
+                  var td = $(document.createElement('td'));
+                  var k = fields[n];
+                  if(k == 'price'){
+                    td.css('text-align', 'right');
+                  }
+                  td.html(account[k]);
+                  tr.append(td);
+                }
+                td = $(document.createElement('td'));
+                var link = $(document.createElement('a'));
+                var href = [location.pathname, "accounts", account._id].join('/');
+                link.attr({'href'       : href,
+                           'data-method': "delete",
+                           'data-remote': "true",
+                           'rel'        : "nofollow"});
+                link.html("[削除]");
+                link.bind('ajax:success', delete_success);
+                link.click(function(e){
+                  $(this).callRemote();
+                  e.preventDefault();
+                });
+                td.append(link);
+                tr.append(td);
+                $("#total_row").before(tr);
+                tr.hide();
+                tr.fadeIn('slow');
+              },
+              error: function(res){
+                var errors = $.parseJSON(res.responseText);
+                for(k in errors){
+                  $("#"+k+'_error').html(errors[k]);
+                }
+                $("#ajax_loader").hide();
+                $("#account_submit").show();
+              }
+             });
+      return false;
+    });
+  var delete_success = function(){
+    var id = this.href.split('/').pop();
+    var node = $("#account_"+ id);
+    node.fadeOut('slow', function(){ node.remove(); });
+  };
+  $("a[data-method=delete]").bind('ajax:success', delete_success);
+});
