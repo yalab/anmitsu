263e1c95a61af2e94fba7bd0e6f4cd55a538008b
yalab <rudeboyjet@gmail.com>
2011-02-17 12:31:01 +0900

    Delete I18n failure message from flash.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,13 @@ class ActiveSupport::TestCase
 end
 class ActionController::TestCase
   include Devise::TestHelpers
+  def flash
+    f = super
+    [:notice, :alert].each do |k|
+      f[k].gsub! /^translation missing: en\./, '' if f[k]
+    end
+    f
+  end
 end
 Mongoid.master.collections.select{ |c| c.name !~ /system\./ }.each { |c| c.drop }
 Dir.glob("#{Rails.root}/test/factories/*.rb"){|factory| require factory }
