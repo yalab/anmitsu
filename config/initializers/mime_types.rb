769f5c278e1d3545ed590c08ac52595915fcd117
yalab <rudeboyjet@gmail.com>
2011-02-17 20:43:21 +0900

    Bill PDF output.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,8 +5,11 @@
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register 'application/pdf', :pdf
 ActionController.add_renderer :pdf do |model, options|
-  # self.content_type = Mime::HTML
-  # self.response_body = render_to_string(:file => "pdfs/#{model.class.name.underscore}")
-  pdfkit = PDFKit.new(render_to_string(:file => "pdfs/#{model.class.name.underscore}"))
-  send_data pdfkit.to_pdf, :filename => model.title + '.pdf'
+  if ENV['NOPDF']
+    self.content_type = Mime::HTML
+    self.response_body = render_to_string(:file => "pdfs/#{model.class.name.underscore}")
+  else
+    pdfkit = PDFKit.new(render_to_string(:file => "pdfs/#{model.class.name.underscore}"))
+    send_data pdfkit.to_pdf, :filename => model.title + '.pdf'
+  end
 end
