6d438a801085f567d7052a1ebc4b7e6c7aeac20f
yalab <rudeboyjet@gmail.com>
2011-02-19 11:02:38 +0900

    Fix templates.

--- lib/ext/action_view/helpers/form_helper.rb
+++ lib/ext/action_view/helpers/form_helper.rb
@@ -1,5 +1,10 @@
 ActionView::Helpers::FormBuilder.module_eval do
   def error_message(attribute)
-    @object.errors[attribute].map{|error| '<span class="error">' + error + '</span>'}.join("<br>").html_safe
+    @object.errors[attribute].map{|error|
+      attr_name = @object.class.human_attribute_name(attribute, :default => attribute.to_s.gsub('.', '_').humanize)
+      message = I18n.t(:"activemodel.errors.full_messages.format", :message => error, :attribute => attr_name)
+      '<span class="alert">' + message + '</span>'
+
+    }.join("<br>").html_safe
   end
 end
