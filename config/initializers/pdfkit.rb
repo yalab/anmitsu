3f855f17968b89b9c4972a70c1164cf7e6ade65b
yalab <rudeboyjet@gmail.com>
2011-02-20 15:07:04 +0900

    PDF size is A4.

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = (Rails.env == 'production') ? Rails.root.join("bin", "wkhtmltopdf") : "/usr/local/bin/wkhtmltopdf"
-  config.default_options = {:page_size        => 'Legal',
+  config.default_options = {:page_size        => 'A4',
                             :print_media_type => true}
 end
