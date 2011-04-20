bc6f7ded74d62c14170d66d6c25ae6b5f49d44fc
yalab <rudeboyjet@gmail.com>
2011-04-20 13:24:42 +0900

    Add mail charset check test.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,13 +1,26 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup{ I18n.locale = :ja }
+  teardown{ I18n.locale = :en }
   context "create" do
     setup do
       @user = User.create(:email => 'test@example.com', :password => (pass = 'password'), :password_confirmation => pass)
+      @mail = ActionMailer::Base.deliveries.last
     end
+
     should "generate information and bank_account" do
-      assert_instance_of Information,    @user.information
+      assert_instance_of Information, @user.information
       assert_instance_of BankAccount, @user.bank_account
+    end
+    should "mail subject encded iso-2022-jp" do
+      assert_match /ISO-2022-JP/, @mail.header[:subject].encoded
+    end
+    should "mail body is encoded iso-2022-jp" do
+      expects = 'Anmitsu\e\$B\$K\$4EPO'.encode("BINARY")
+      body = @mail.body.encoded.to_s.encode("BINARY")
+      assert_match /#{expects}/, body
     end
   end
 end
