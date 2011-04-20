96b1d7f8aa87d024cb9e592c122864f8381f3e02
yalab <rudeboyjet@gmail.com>
2011-04-20 19:18:48 +0900

    Upgrade mongoid.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
-gem 'bson_ext', '~>1.2.2'
-gem 'mongoid', '~>2.0.0.rc'
+gem 'bson_ext', '~>1.3.0'
+gem 'mongoid', '~>2.0.1'
 gem 'capistrano'
 gem 'devise'
 gem 'stateflow'
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,10 @@
 class Account
   include ActionView::Helpers::NumberHelper
   include Mongoid::Document
-  field(:content, :type => String)  || validates(:content, :presence => true)
-  field(:price,   :type => Integer) || validates(:price,   :presence => true, :numericality => {:only_integer => true})
-
+  field :content, :type => String
+  field :price,   :type => Integer
+  validates :content, :presence => true
+  validates :price,   :presence => true, :numericality => {:only_integer => true}
   field(:note,    :type => String)  || nil
   embedded_in :item, :inverse_of => :accounts
 
