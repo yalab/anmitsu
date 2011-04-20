0ea51082b2d697ac001e773aac7b40bcda9bd65e
yalab <rudeboyjet@gmail.com>
2011-04-20 16:05:43 +0900

    Upgrade mail gem.

--- lib/ext/mail/field.rb
+++ lib/ext/mail/field.rb
@@ -1,3 +1,4 @@
+require 'mail'
 Mail::Field.module_eval do
   def new_field_with_subject_charset(name, value, charset)
     field = new_field_without_subject_charset(name, value, charset)
--- lib/ext/mail/fields/unstructured_field.rb
+++
@@ -1,9 +0,0 @@
-require 'mail'
-module Mail
-  class UnstructuredField
-    def encode(value)
-      value.encode!(charset) if defined?(Encoding) && charset
-      (value.not_ascii_only? ? [value].pack("M").gsub("=\n", '') : value).gsub("\r", "=0D").gsub("\n", "=0A")
-    end
-  end
-end
