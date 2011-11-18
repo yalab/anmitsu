bcf700b499161e448129de1afaeab19a8a5d3591
yalab <rudeboyjet@gmail.com>
2011-11-18 18:07:20 +0900

    Move nl2br method define class.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def nl2br(stringish)
+    html_escape(stringish).gsub(/\n/, '<br>').html_safe
+  end
 end
--- lib/ext/action_view/helpers/raw_output_helper.rb
+++
@@ -1,5 +0,0 @@
-module ActionView::Helpers::OutputSafetyHelper
-  def nl2br(stringish)
-    html_escape(stringish).gsub(/\n/, '<br>').html_safe
-  end
-end
