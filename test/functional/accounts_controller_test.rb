a7def74a69c3042542101e1f1881856e7e57909e
yalab <rudeboyjet@gmail.com>
2013-06-18 23:35:27 +0900

    Fixed fixture_upload_file.

--- test/functional/accounts_controller_test.rb
+++ test/functional/accounts_controller_test.rb
@@ -46,7 +46,7 @@ class AccountsControllerTest < ActionController::TestCase
   context "on POST :create_all" do
     setup do
       @count = @item.accounts.count
-      file = fixture_file_upload(Rails.root.join(*%w|test fixtures accounts.csv|))
+      file = fixture_file_upload('accounts.csv')
       post :create_all, item_id: @item.to_param, format: 'csv', account: {file: file}
     end
 
